5d90ccdb1e8f6a9d94d4a9fcdfcbf60ca3a1cc39
Clint Milner <clinton.milner@gmail.com>
2018-08-28 20:47:59 +0100

    WEBPACK STARTER

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>My React App</h1>
+            </div>
+        );
+    }
+}
